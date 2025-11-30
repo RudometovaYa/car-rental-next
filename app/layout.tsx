ae64b3e75a7f08b5f59daff7cb44006fc942be2a
Yana Rudometova <rudometova.ya@gmail.com>
2025-11-30 22:19:21 +0200

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,37 @@
 import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
-/* import './globals.css'; */
+import { Manrope } from 'next/font/google';
 import Header from '@/components/Header/Header';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
-const robotoFont = Roboto({
-  variable: '--font-roboto',
+const manropeFont = Manrope({
+  variable: '--font-manrope',
   subsets: ['latin'],
   display: 'swap',
-  weight: ['400', '700'],
+  weight: ['500', '700'],
 });
 
+export const metadataBase = new URL('https://car-rental-next-three.vercel.app');
+
 export const metadata: Metadata = {
-  title: 'Car Rental',
-  description: 'Find and book rental cars easily',
+  title: 'Car Rental | Find your perfect car',
+  description:
+    'Find and book reliable rental cars for any journey. Flexible prices, popular brands, and easy booking.',
+  openGraph: {
+    title: 'Car Rental | Find your perfect car',
+    description:
+      'Browse popular car brands, compare prices, and book your ideal rental car in a few clicks.',
+    url: 'https://car-rental-next-three.vercel.app',
+    siteName: 'Car Rental',
+    images: [
+      {
+        url: '/car rental.png',
+        width: 1440,
+        height: 768,
+        alt: 'Car Rental website preview',
+      },
+    ],
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -23,7 +41,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={robotoFont.variable}>
+      <body className={manropeFont.variable}>
         <TanStackProvider>
           <div className="layout">
             <Header />
